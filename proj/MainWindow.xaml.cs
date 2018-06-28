97ed72a76187d37f3e29f98d77c709a44aba3637
Алексей Покора <pokoraalex477@gmail.com>
2018-06-28 16:31:11 +0300

    Add project files.

---
+++ proj/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace proj
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            cmb1.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+            cmb2.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+        }
+
+        private void SynchronizeWith(object sender, RoutedEventArgs e)
+        {
+            object temp = richTextBox.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            button.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = richTextBox.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            button1.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = richTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            button2.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            
+            temp = richTextBox.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmb1.SelectedItem = temp;
+            temp = richTextBox.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmb2.Text = temp.ToString();
+        }
+
+        private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmb1.SelectedItem != null)
+                richTextBox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, cmb1.SelectedItem);
+        }
+
+        private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            richTextBox.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmb2.Text);
+
+        }
+
+        private void Open_Executed(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+                TextRange range = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                range.Load(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Save_Executed(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                TextRange range = new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd);
+                range.Save(fileStream, DataFormats.Rtf);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label.Content = "Тыц.";
+        }
+    }
+}
